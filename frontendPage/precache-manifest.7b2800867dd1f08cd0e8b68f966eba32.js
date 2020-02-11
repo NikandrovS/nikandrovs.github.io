84f52dbee4ff2f5e4ba2fbe9516d2d932144a659
NikandrovS <39054635+NikandrovS@users.noreply.github.com>
2020-02-11 23:49:02 +0300

    Add files via upload

---
+++ frontendPage/precache-manifest.7b2800867dd1f08cd0e8b68f966eba32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2090cb60200646bb3835d2563c0b4b60",
+    "url": "/frontendPage/index.html"
+  },
+  {
+    "revision": "a867082baf757167ba4c",
+    "url": "/frontendPage/static/css/main.651be6f3.chunk.css"
+  },
+  {
+    "revision": "43f7c4002746f14bec0c",
+    "url": "/frontendPage/static/js/2.bf4337d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/frontendPage/static/js/2.bf4337d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a867082baf757167ba4c",
+    "url": "/frontendPage/static/js/main.56e39b9a.chunk.js"
+  },
+  {
+    "revision": "c734aee78ebf9567d665",
+    "url": "/frontendPage/static/js/runtime-main.c4f1e102.js"
+  }
+]);
